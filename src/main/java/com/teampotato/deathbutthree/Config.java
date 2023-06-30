6a523b9e079b66620540eeda952893bfa25eb391
RocketCreeper <82942121+AlanightCreeper@users.noreply.github.com>
2023-06-30 13:29:36 +0800

    Update Config.java

--- src/main/java/com/teampotato/deathbutthree/Config.java
+++ src/main/java/com/teampotato/deathbutthree/Config.java
@@ -8,11 +8,13 @@ import java.util.List;
 public class Config {
     public static final ForgeConfigSpec configSpec;
     public static final ForgeConfigSpec.ConfigValue<List<? extends String>> bosses;
+    public static final ForgeConfigSpec.IntValue maxDeathAmount;
 
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.push("DeathButThree");
         bosses = builder.defineList("ValidBosses", Lists.newArrayList("cataclysm:ignis"), o -> o instanceof String);
+        maxDeathAmount = builder.comment("The max times of death. Default value is 3").defineInRange("maxDeathAmount", 3, 1, Integer.MAX_VALUE);
         builder.pop();
         configSpec = builder.build();
     }
